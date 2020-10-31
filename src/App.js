7fe21c98db4041f744d6c5fc042c083edbee711c
Fred Zhou <fredpaw@gmail.com>
2020-10-31 22:59:16 +1100

    Headline component and test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import Header from './components/header/header';
+import Headline from './components/headline/headline';
 
 function App() {
   return (
-    <Header />
+    <div className="App">
+      <Header />
+      <section className="main">
+        <Headline header="Posts" desc="Click the button to get posts!" />
+      </section>
+    </div>
   );
 }
 
---
+++ src/components/headline/headline.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Headline from './headline';
+import { findByTestAttr } from '../../../utils';
+
+const setup = (props = {}) => shallow(<Headline {...props} />)
+
+describe('Headline Component', () => {
+  describe('Have props', () => {
+    let wrapper;
+
+    beforeAll(() => {
+      const props = {
+        header: 'Test Header',
+        desc: 'Test Desc',
+      }
+      wrapper = setup(props);
+    })
+
+    it('Should render without errors', () => {
+      const component = findByTestAttr(wrapper, 'HeadlineComponent');
+      expect(component.length).toBe(1);
+    })
+
+    it('Should render a H1', () => {
+      const h1 = findByTestAttr(wrapper, 'header');
+      expect(h1.length).toBe(1);
+    })
+
+    it('Should render a Desc', () => {
+      const desc = findByTestAttr(wrapper, 'desc');
+      expect(desc.length).toBe(1);
+    })
+  });
+
+  describe('Have NO props', () => {
+    let wrapper;
+
+    beforeAll(() => {
+      wrapper = setup();
+    })
+
+    it('Should not render', () => {
+      const component = findByTestAttr(wrapper, 'HeadlineComponent');
+      expect(component.length).toBe(0)
+    })
+  })
+  
+})
