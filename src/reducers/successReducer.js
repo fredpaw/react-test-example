9d9824bf0fd283c5c09dce186c96423fb2310cd3
Fred Zhou <fredpaw@gmail.com>
2020-11-02 00:41:21 +1100

    Add redux to app

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,14 @@
+const initialState = {}
+
+const successReducer =  (state = initialState, action) => {
+  switch (action.type) {
+    case 'value':
+      
+      break;
+  
+    default:
+      return state;
+  }
+};
+
+export default successReducer;
